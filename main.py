0731baeebe9dcc40a9399a17e0c1a25a4d9f2816
MrKomugiko <Mr.Komugiko@gmail.com>
2019-09-20 01:55:10 +0200

    Dodanie zapisu i odczytu danych rankingowych z pliku

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pytesseract
 from PIL import Image
-import json
+import json, datetime
+import pickle
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 # Utworzenie osobnej listy zawierającej nick i liczbe punktow + ranking po posortowaniu
@@ -54,8 +55,9 @@ def wyodrebnij_dane_z_pliku(img):
                     ranking[znalezioneNicki][1] = []
                     ranking[znalezioneNicki][1] += [temp]
         znalezioneNicki += 1
+    zaktualizuj_liste_dodanych_screenow(img)
     print("file created")
-# --------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------   ---------------------------------------------------------------
 #TODO: Wyodrębnienie nazw użytkowników i sprawdzanie czy na liście pojawił się jakiś nowy gracz (?)
 # skąd program ma wiedzieć że to nowy gracz ?... !! jeżeli liczba dopasowań będzie mniejsza niż zakłada to pętla na początku,
 # wówczas poprosi nas o wprowadzenie nazwy użytkownika któy się pojawił ...
@@ -88,18 +90,41 @@ def dodaj_dane_z_pliku(img):
                     ranking[znalezioneNicki][1] += [text_podzielony[i + 2]]
 
         znalezioneNicki += 1
+    zaktualizuj_liste_dodanych_screenow(img)
     print("file updated succesfully")
 # --------------------------------------------------------------------------------------------------------------
 def wyswietl_ranking():
     for i, elem in enumerate(ranking):
         print(ranking[i])
+
+
+def zaktualizuj_ranking(ranking_data):
+    with open("Ranking_data", "wb") as fp:  # Pickling
+        pickle.dump(ranking, fp)
+
+def zaktualizuj_liste_dodanych_screenow(img):
+    text_file = open("Screens_used", "a+")
+    text_file.write(f"{img}\t{datetime.datetime.now()}\n")
+    text_file.close()
+
+def zaladuj_dane_rankungu():
+    with open("Ranking_data", "rb") as fp:   # Unpickling
+        ranking = pickle.load(fp)
+    print("Pomyślnie zostały załadowane dane rankingu.")
+    return ranking
+
 # --------------------------------------------------------------------------------------------------------------
-wyodrebnij_dane_z_pliku("Screenshot_20190919-161551_Activy.jpg")
-dodaj_dane_z_pliku("Screenshot_20190919-212751_Activy.jpg")
-dodaj_dane_z_pliku("Screenshot_20190919-231233_Activy.jpg")
-wyswietl_ranking()
+
+#wyodrebnij_dane_z_pliku("Screenshot_20190919-161551_Activy.jpg")
+#dodaj_dane_z_pliku("Screenshot_20190919-212751_Activy.jpg")
+#dodaj_dane_z_pliku("Screenshot_20190919-231233_Activy.jpg")
+#wyswietl_ranking()
+#zaktualizuj_ranking(ranking)
+
+ranking = zaladuj_dane_rankungu()
+print(ranking[0])
 # --------------------------------------------------------------------------------------------------------------
-''' 
+'''
 list = ranking
 json.dumps(list)
 print(json.dumps(list))
@@ -107,3 +132,20 @@ print(json.dumps(list))
 #TODO: Zapisywanie i odczytywanie danych do pliku tekstowego w formacie json(?).
 
 #TODO: Jakieś wykresy by się też przydały xD
+
+#TODO: Przeglądanie katalogu i automatyczne przenoszenie pliku jpg do folderu aplikacji,
+# lista wcześniej zainmportowanych screenów, i aktualizowanie tylko o nowe (?)
+
+'''
+f=open("Ranking_data", "r")
+f1 = f.readlines()
+for x in f1:
+    print(x)
+
+nowy_ranking_test = [[]]
+nowy_ranking_test=list(f1)
+f.close()
+print(ranking)
+print(nowy_ranking_test)
+
+'''
