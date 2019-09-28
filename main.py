3903f8d65bfaaf17f93e0cdd62a2fa010c389975
MrKomugiko <Mr.Komugiko@gmail.com>
2019-09-28 22:59:12 +0200

    zapis danych z nowych screenow bezposrednio do pliku tekstowego, do celów póżniejszego szybszego wykorzystania

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-#coding=gbk
+# coding=gbk
 
-import pytesseract, json, datetime,time, pickle, os, shutil
+import pytesseract, json, datetime, time, pickle, os, shutil
 from PIL import Image
+
 # Wskazanie lokalizacji zainstalowanego tesseraktu (od google)
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
 
 # Predefiniowana lista nick車w kt車re maj? by? dost?pne w rankingu
-l_nick = ["Herman", "Pawel", "Stanislaw", "Dariusz", "Tomasz", "Matgosia", "A_Jak", "Janusz", "Ragnar", "marjac"]
+l_nick = ["Herman", "Pawel", "Stanislaw", "Dariusz", "Tomasz", "Matgosia", "A_Jak", "Janusz", "Ragnar", "marjac",
+          "Slawomir", "HeRmAn"]
 l_ranking = [[]]
 l_ranking_pozycja = [[]]
 
-#inicjalizacja listy nick車w recznie (dodanie pocz?tkowych slot車w(? xD) )
-for i,nick in enumerate(l_nick):
+# inicjalizacja listy nick車w recznie (dodanie pocz?tkowych slot車w(? xD) )
+for i, nick in enumerate(l_nick):
     l_ranking.append([])
     l_ranking[i].append(nick)
     l_ranking[i].append([])
@@ -29,6 +31,15 @@ def getTextFromImage(imgUrl):
     zaktualizuj_liste_dodanych_screenow(imgUrl)
     return text_podzielony
 
+
+def getTextFromFile(fileUrl):
+    with open(f"Screens_data\\{fileUrl}", "r") as text:
+        line = text.read()
+    text_podzielony = line.split()
+    print(text_podzielony)
+    return text_podzielony
+
+
 # Przekonwertowanie tekstu na dane punktow i nick車w, nastepnie dodanie do listy
 def updateRanking(text):
     # Pozycja wedlug indeksu zdeterminowana jest kolejnoscia wystapien nicka w liscie l_nick.
@@ -45,7 +56,7 @@ def updateRanking(text):
         # Sprawdzenie czy wybrany wyraz jest nickiem z listy,
         #   je?eli tak, w車wczas sprawdzany jest nast?pny element i kolejny pod kontem poprawno?ci/
         #   dopasowania do punktacjiw rankingu, (s?owa, znaki i liczby poni?ej 1k pkt s? eliminowane)
-        if elem=="oj," or elem == "Aiak" or elem=="Ajak":
+        if elem == "oj," or elem == "Aiak" or elem == "Ajak" or elem == "Alsk":
             elem = "A_Jak"
         if elem in l_nick:
             nick_index = l_nick.index(elem)
@@ -64,20 +75,52 @@ def updateRanking(text):
             pozycja += 1
 
     # Przekazanie rankingu w celu jego aktualizacji
-    zaktualizuj_plik("Ranking_data",l_ranking)
+    zaktualizuj_plik("Ranking_data", l_ranking)
     zaktualizuj_plik("Ranking_position_data", l_ranking_pozycja)
-    print("Ranking zostal pomyslnie zaktualizowany")
+
+
+def updateRanking_TEST(text):
+    for i, item in enumerate(l_nick):
+        print(item)
+        item = "'" + item + "'"
+        if item not in text and item != "A_Jak":
+            l_ranking_TEST[i][1] += [""]
+            l_ranking_pozycja_TEST[i][1] += [""]
+    pozycja = 1
+    for i, elem in enumerate(text):
+        elem = elem[1:-2]
+        punkty = 0
+        if elem == "oj," or elem == "Aiak" or elem == "Ajak" or elem == "Alsk":
+            elem = "A_Jak"
+        if elem in l_nick:
+            nick_index = l_nick.index(elem)
+            if (str.isdigit(text[i + 1][1:-2])):
+                if int(text[i + 1][1:-2]) > 1000:
+                    punkty = text[i + 1][1:-2]
+                elif (str.isdigit(text[i + 2][1:-2])):
+                    if int(text[i + 2][1:-2]) > 1000:
+                        punkty = text[i + 2][1:-2]
+            else:
+                if str.isdigit(text[i + 2][1:-2]):
+                    if int(text[i + 2][1:-2]) > 1000:
+                        punkty = text[i + 2][1:-2]
+            l_ranking_TEST[nick_index][1] += [punkty]
+            l_ranking_pozycja_TEST[nick_index][1] += [pozycja]
+            pozycja += 1
+
 
 # Aktualizowanie pliku 'Ranking_data' o dane rankingu
 def zaktualizuj_plik(file_name, ranking_data):
     with open(f"{file_name}", "wb") as fp:  # Pickling
         pickle.dump(ranking_data, fp)
 
+
 # Zwraca aktualny ranking pobranych z pliku 'Ranking_data'
-def zaladuj_dane_rankungu_z_pliku(file_name):
+def zaladuj_dane_z_pliku(file_name):
     with open(f"{file_name}", "rb") as fp:  # Unpickling
         ranking = pickle.load(fp)
     return ranking
+
 
 # Zwraca list? wszystkich wcze?niej wykorzystzanych screenow, pobieranych z pliku 'Screens_used'
 def pobierz_liste_dodanych_screenow():
@@ -89,11 +132,13 @@ def pobierz_liste_dodanych_screenow():
             line = fp.readline()
     return lista
 
+
 # Dodawanie do pliku 'Screens_used' nazw screen車w kt車re zosta?y ju? przetworzone
 def zaktualizuj_liste_dodanych_screenow(img):
     text_file = open("Screens_used", "a+")
     text_file.write(f"{img[8:]}\t{datetime.datetime.now()}\n")
     text_file.close()
+
 
 # PRzeszukanie katalogu Screens pod k?tem nowych element車w
 def sprawdz_nowe_screeny():
@@ -105,12 +150,14 @@ def sprawdz_nowe_screeny():
             nowe_lista += [all_screens]
     return nowe_lista
 
+
 # Wy?wietlenie ca??go rankingu dla wszystkich elementow(nick車w)
 def pokaz_liste_ranking():
     i = 0
     while i < len(l_ranking):
         print(l_ranking[i])
         i += 1
+
 
 # Przeszukiwanie folderu Dokumenty (domyslego katalogu na ktory importowane s? screeny) \
 #   w celu znalezienia nowych elementow nie zaimportowanych do aplikacji,
@@ -121,64 +168,72 @@ def sprawdz_i_przenies_nowe_screeny():
     counter = 0
     for files in source:
         if files.endswith(".jpg") and files not in pobierz_liste_dodanych_screenow():
-            shutil.move(f"C:\\Users\MrKom\Documents\{files}",destination)
-            counter +=1
+            shutil.move(f"C:\\Users\MrKom\Documents\{files}", destination)
+            counter += 1
     if counter == 0:
         print("Brak nowych elementow.")
     else:
         print(f"Zostaly dodane {counter} nowe screeny.")
 
-def TEST_SHOW_RANKINGS(type=""):
 
-   if type=="clearRead":
+def TEST_SHOW_RANKINGS(type=""):
+    if type == "clearRead":
         # Wy?wietlanie rankingu z pliku => posortowane wed?ug pozycji 1-7
         print("")
         x = len(l_nick)
-        for index,item in enumerate(l_nick):
-            print('{:.>10}'.format(l_ranking[index][0]), end="")
-            for i,elem in enumerate(l_ranking[index][1]):
-                print('{:>5}'.format(l_ranking[index][1][i]),end="")
+        for index, item in enumerate(l_nick):
+            print('{:.>10}'.format(l_ranking[index][0]), end=" ")
+            for i, elem in enumerate(l_ranking[index][1]):
+                print('{:>5}'.format(l_ranking[index][1][i]), end="")
             print("")
 
         print("")
         x = len(l_nick)
-        for index,item in enumerate(l_nick):
-            print('{:.>10}'.format(l_ranking_pozycja[index][0]), end="")
-            for i,elem in enumerate(l_ranking_pozycja[index][1]):
-                print('{:^2}'.format(l_ranking_pozycja[index][1][i]),end="")
+        for index, item in enumerate(l_nick):
+            print('{:.>10}'.format(l_ranking_pozycja[index][0]), end=" ")
+            for i, elem in enumerate(l_ranking_pozycja[index][1]):
+                print('{:^2}'.format(l_ranking_pozycja[index][1][i]), end="")
             print("")
 
-   elif type=="copyReady":
+    elif type == "copyReady":
         x = len(l_nick)
-        for index,item in enumerate(l_nick):
+        for index, item in enumerate(l_nick):
             print('{:}'.format(l_ranking[index][0]), end=" ")
-            for i,elem in enumerate(l_ranking[index][1]):
-                print('{:}'.format(l_ranking[index][1][i]),end=" ")
+            for i, elem in enumerate(l_ranking[index][1]):
+                print('{:}'.format(l_ranking[index][1][i]), end=" ")
             print("")
 
         print("")
 
         x = len(l_nick)
-        for index,item in enumerate(l_nick):
+        for index, item in enumerate(l_nick):
             print('{:}'.format(l_ranking_pozycja[index][0]), end=" ")
-            for i,elem in enumerate(l_ranking_pozycja[index][1]):
-                print('{:}'.format(l_ranking_pozycja[index][1][i]),end=" ")
+            for i, elem in enumerate(l_ranking_pozycja[index][1]):
+                print('{:}'.format(l_ranking_pozycja[index][1][i]), end=" ")
             print("")
 
-   else:
-       print("jako parametr podaj <clearRead> jako lanie sformatowane i przejrzyste,\nalbo <copyReady> elementy oddzielone spacjami, \nwystarczy wklejenie specjalne do excela z uwzglednieniem spacjii")
+    else:
+        print(
+            "jako parametr podaj <clearRead> jako lanie sformatowane i przejrzyste,\nalbo <copyReady> elementy oddzielone spacjami, \nwystarczy wklejenie specjalne do excela z uwzglednieniem spacjii")
 
 
-#TODO: Kopia zapasowa przed wgraniem nowych screen車w, w celu skorygowania b??d車w i przywr車cenia wczesniejszej wersjii
+def wyeksportuj_dane_do_pliku_tekstowego(imgUrl):
+    image = Image.open(imgUrl)
+    text = pytesseract.image_to_string(image)
+    text_podzielony = text.split()
+    screens_text_data.append(text_podzielony)
+    return screens_text_data
 
+# TODO: Kopia zapasowa przed wgraniem nowych screen車w, w celu skorygowania b??d車w i przywr車cenia wczesniejszej wersjii
 
 sprawdz_i_przenies_nowe_screeny()
 
 # Sprawdzenie kt車re screeny zosta?y ju? wcze?niej zaimportowane
 if pobierz_liste_dodanych_screenow() != []:
     # Pobranie do pami?ci aktualnych danych z pliku
-    l_ranking = zaladuj_dane_rankungu_z_pliku("Ranking_data")
-    l_ranking_pozycja = zaladuj_dane_rankungu_z_pliku("Ranking_position_data")
+    l_ranking = zaladuj_dane_z_pliku("Ranking_data")
+    l_ranking_pozycja = zaladuj_dane_z_pliku("Ranking_position_data")
+    screens_text_data = zaladuj_dane_z_pliku("Screens_Text_Data")
 
 # Sprawdzenie, czy zosta?y dodane nowe screeny
 if not sprawdz_nowe_screeny:
@@ -187,13 +242,28 @@ else:
     count_new_screens = len(sprawdz_nowe_screeny())
 
 # Proces aktualizowania rankingu o nowe screeny, z prostym wskaznikiem post?pu
-for i,imgUrl in enumerate(sprawdz_nowe_screeny()):
+
+for i, imgUrl in enumerate(sprawdz_nowe_screeny()):
     print(f"[ PROGRESS : {i}/{count_new_screens} ]")
-    #print(getTextFromImage(f"Screens/{imgUrl}"))
     updateRanking(getTextFromImage(f"Screens/{imgUrl}"))
+    zaktualizuj_plik("Screens_Text_Data", wyeksportuj_dane_do_pliku_tekstowego(f"Screens\\{imgUrl}"))
 
 TEST_SHOW_RANKINGS("clearRead")
 
+'''   
+if pobierz_liste_dodanych_screenow() != []:
+    for i, elem in enumerate(pobierz_liste_dodanych_screenow()):
+        print(f"Uploading file {i+1} of {len(pobierz_liste_dodanych_screenow())}")
+        zaktualizuj_plik("Screens_Text_Data", wyeksportuj_dane_do_pliku_tekstowego(f"Screens\\{elem}"))
 
+        i = 0
+        while i < len(screens_text_data):
+            print(screens_text_data[i])
+            print("")
+            i += 1
+'''
 
+for i, elem in enumerate(screens_text_data):
+    print('{:>3}'.format(i), elem)
 
+print("END")
